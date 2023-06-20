cbd4a05a8620f28f166669d82042003c0670e687
lapaoleksiy <136694985+lapaoleksiy@users.noreply.github.com>
2023-06-20 14:55:35 +0700

    Update documentation

---
+++ yellow_dog_elephant.rs
@@ -0,0 +1,35 @@
+fn main() {
+    println!("The Art of Healing");
+
+    // Recognize and Treat Dis-ease
+    println!("1. Acknowledge the existence of the dis-ease and assess its severity.");
+    println!("2. Create a comprehensive treatment plan, combining both traditional and non-traditional approaches.");
+    println!("3. Encourage your patient to rest and reduce stress to facilitate fast healing.");
+    println!("4. Look for underlying causes and treat them, in addition to the acute symptoms.");
+    println!("5. Supply comfortable, nurturing and healing environments that will aid recovery.");
+    println!("6. Monitor progress and adjust the treatment plan accordingly.");
+
+    // Nurture
+    println!("7. Provide necessary nutrition. Utilize dietary supplements as needed.");
+    println!("8. Offer physical support and comfort when appropriate.");
+    println!("9. Administer natural remedies, such as herbs, oils and teas.");
+    println!("10. Encourage emotional expression and provide emotional support as needed.");
+    println!("11. Promote physical activity and engage in physical activities as appropriate.");
+    println!("12. Promote spiritual growth and spiritual healing, as part of a comprehensive plan.");
+
+    // Educate
+    println!("13. Teach your patient to recognize and understand the disease and its progression.");
+    println!("14. Disseminate information about the healing process.");
+    println!("15. Provide insight into lifestyle changes needed to promote healing.");
+    println!("16. Increase awareness of the importance of self-care and self-responsibility.");
+    println!("17. Emphasize the importance of spiritual growth and development.");
+    println!("18. Encourage the use of alternative approaches that optimize the natural healing process.");
+
+    // Remain Aware
+    println!("19. Stay abreast of advancements in healing modalities.");
+    println!("20. Utilize current research and evidence-based approaches.");
+    println!("21. Share your knowledge and expertise to benefit the greater good.");
+    println!("22. Maintain an open dialogue with patients and/or their loved ones.");
+    println!("23. Develop and nurture relationships with other healing professionals.");
+    println!("24. Remain open to new ideas and suggestions from colleagues and patients.");
+}
