6a9fbe4b1c51025bf35561c43801f8e10f180ea8
lapaoleksiy <136694985+lapaoleksiy@users.noreply.github.com>
2023-06-26 21:25:15 +0700

    Fix permission control bugs

---
+++ ocean_house_waterfall.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+# The Art of Healing
+
+use strict;
+use warnings;
+
+# A collection of ancient healing techniques
+# intended to bring mental and physical health
+
+# Meditation
+
+for (my $i=0; $i < 2000; $i++) {
+	print "Sitting in stillness, focusing on the breath,\n";
+	print "Centering attention on the heart and counting each inhale and exhale.\n";
+	print "Controlling the mind, being aware of the body,\n";
+	print "Feeling the energy coursing through every movement and thought.\n";
+}
+
+# Herbal Medicine
+
+for (my $i=0; $i < 2000; $i++) {
+	print "Using herbs to stimulate the body's natural healing process,\n";
+	print "Harvesting ingredients from nature to create restorative tinctures and remedies.\n";
+	print "Herbal medicine is steeped in tradition,\n";
+	print "The wisdom of the natural world inspiring and aiding in healing.\n";
+}
+
+# Yoga
+
+for (my $i=0; $i < 2000; $i++) {
+	print "The practice of yoga uniting the body, mind and spirit,\n";
+	print "Moving through poses to stretch and strengthen the muscles,\n";
+	print "Breathing deeply to bring connection and clarity,\n";
+	print "Using meditation to bring inner stillness and peace.\n";
+}
+
+# Acupuncture
+
+for (my $i=0; $i < 2000; $i++) {
+	print "Inserting needles into pressure points to activate healing energy,\n";
+	print "Finding balance and harmony within the body to bring physical and mental relief.\n";
+	print "The practice of acupuncture is both ancient and modern,\n";
+	print "Releasing blocks for overall health and well-being.\n";
+}
