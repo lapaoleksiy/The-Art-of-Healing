aeb33ffecf566bd8d1ac5ecc8edc433c14f499b2
lapaoleksiy <136694985+lapaoleksiy@users.noreply.github.com>
2023-07-17 06:47:23 +0700

    Add support for multiple languages

---
+++ flower_banana_king.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // Variables
+    int counter = 0;
+
+    // The Art Of Healing
+    printf("The Art Of Healing\n");
+    printf("\n");
+
+    // Loop to Print 2000 Lines
+    while(counter < 2000)
+    {
+        printf("The art of healing is more than medicines and bandages. It is a combination of knowledge, dedication, and compassion that leads to a successful recovery.\n");
+        counter++;
+    }
+
+    return 0;
+}
