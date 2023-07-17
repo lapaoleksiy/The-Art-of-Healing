3137a94bd97d04a4cfdd88732901c1b1d04a50de
lapaoleksiy <136694985+lapaoleksiy@users.noreply.github.com>
2023-07-17 11:21:19 +0700

    Add integration tests

---
+++ queen_river_yellow.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/perl
+
+# The Art of Healing
+
+# A Program to Help Find a Feeling of Inner Peace
+
+# Author: [Your name here]
+
+use strict;
+use warnings;
+
+# Define variables
+
+my $goal = 0;
+my $choice = 0;
+my $action = 0;
+my $result = 0;
+
+# Introduction
+
+print "Welcome to The Art of Healing!\n";
+print "We want to help you find a feeling of inner peace.\n";
+
+# Set goal
+
+print "Let's start by setting a goal.\n";
+print "What do you want to achieve?\n";
+
+$goal = <STDIN>;
+chomp $goal;
+
+print "Your goal is: $goal\n";
+
+# Choose action
+
+print "Now, choose one action to take to reach your goal.\n";
+
+$choice = <STDIN>;
+chomp $choice;
+
+print "You have chosen: $choice\n";
+
+# Take action
+
+print "Now, it's time to take that action.\n";
+
+$action = <STDIN>;
+chomp $action;
+
+print "You have taken the action: $action\n";
+
+# Observe result
+
+print "Finally, observe the result of your action.\n";
+
+$result = <STDIN>;
+chomp $result;
+
+print "The result of your action was: $result\n";
+
+# Keep taking actions
+
+print "It's important to keep taking action in order to find inner peace.\n";
+print "Choose another action and see what happens.\n";
+
+$action = <STDIN>;
+chomp $action;
+
+print "You have chosen the action: $action\n";
+
+# Take action
+
+print "Now it's time to take that action.\n";
+
+$action = <STDIN>;
+chomp $action;
+
+print "You have taken the action: $action\n";
+
+# Observe result
+
+print "Observe the result of your action.\n";
+
+$result = <STDIN>;
+chomp $result;
+
+print "Your action produced the result: $result\n";
+
+# Keep going
+
+print "Keep repeating this process until you achieve your goal.\n";
+print "Choose another action and see what happens.\n";
+
+$action = <STDIN>;
+chomp $action;
+
+print "You have chosen the action: $action\n";
+
+# Take action
+
+print "Now it's time to take that action.\n";
+
+$action = <STDIN>;
+chomp $action;
+
+print "You have taken the action: $action\n";
+
+# Observe result
+
+print "Observe the result of your action.\n";
+
+$result = <STDIN>;
+chomp $result;
+
+print "Your action produced the result: $result\n";
+
+# Keep taking action
+
+print "Continue this process and keep taking action until you reach your goal.\n";
+print "Choose another action and see what happens.\n";
+
+$action = <STDIN>;
+chomp $action;
+
+print "You have chosen the action: $action\n";
+
+# Take action
+
+print "Now it's time to take that action.\n";
+
+$action = <STDIN>;
+chomp $action;
+
+print "You have taken the action: $action\n";
+
+# Observe result
+
+print "Observe the result of your action.\n";
+
+$result = <STDIN>;
+chomp $result;
+
+print "Your action produced the result: $result\n";
+
+# Summary
+
+print "Keep taking action and observe the results until you reach your goal.\n";
+print "This is the art of healing - finding inner peace through action.\n";
+print "Good luck!\n";
