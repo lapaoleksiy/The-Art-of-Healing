8f17b2b34f38d66ad26816f26d7499296c805005
lapaoleksiy <136694985+lapaoleksiy@users.noreply.github.com>
2023-07-17 19:20:18 +0700

    Improve code readability

---
+++ ocean_king_unicorn.js
@@ -0,0 +1,25 @@
+let artOfHealing = { 
+    definition : "The art of healing is a practice of mediating and nurturing the body, mind, and spirit of an individual to promote a healthier lifestyle.", 
+    healingProcess: 
+    { 
+        step1: "Begin to become aware of your physical, mental, emotional, and spiritual self. Make an effort to understand how these elements interact with one another.",
+        step2: "Adopt a positive attitude and approach to life. Adopting an attitude of compassion, openness, and kindness can lead to the healing process.",
+        step3: "Focus on healthy practices, such as eating nutritious foods, engaging in physical activities or exercises, taking regular breaks, and allowing yourself adequate rest.",
+        step4: "Connect with nature and take part in activities that bring you joy. Make time for hobbies or activities you love, and seek out activities that bring you closer to nature.",
+        step5: "Focus on self-care. Make sure you are getting enough sleep, eating healthy foods, and engaging in activities and practices that bring you joy and satisfaction.",
+        step6: "Develop a spiritual practice that nourishes your feelings of compassion and connectedness. This can be as simple as having a daily meditation practice or as involved as studying a particular spiritual tradition.",
+        step7: "Engage in therapy and counseling. Talking to a professional can help you address any negative emotions or behaviors that may be blocking the healing process.",
+    }, 
+
+    healingPractices : 
+    {
+        mental: "Engage in positive thinking and visualisation. Make sure to focus on what you do have in your life and take a moment to acknowledge your strengths and successes.",
+        physical: "Engage in practices that keep your physical body in balance. This includes practices such as yoga, tai chi, stretching, and massage.",
+        emotional: "Focus on developing self-compassion. This means being kind and patient to yourself, recognizing and validating your feelings, and having an understanding of yourself and your needs.",
+        spiritual: "Focus on developing a sense of connectedness. This includes practices such as prayer, meditation, journaling, and contemplation."
+ 
+    }, 
+    conclude: "By engaging in these practices, you can take a holistic approach to your healing journey. Whether you choose to focus on physical, mental, emotional, or spiritual healing, the key is to be patient, kind to yourself, and open to change."
+} 
+
+console.log(artOfHealing);
