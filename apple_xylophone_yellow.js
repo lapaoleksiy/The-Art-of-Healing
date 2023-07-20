effcda286bad45a08945297d0301085a10cb8b41
lapaoleksiy <136694985+lapaoleksiy@users.noreply.github.com>
2023-07-20 14:56:00 +0700

    Update API documentation

---
+++ apple_xylophone_yellow.js
@@ -0,0 +1,32 @@
+//The Art of Healing
+//A Guide to Empathy-Based Self-Care
+
+//1. Understand the importance of self-care
+//Self-care is the act of taking care of ourselves mentally, emotionally, and physically. It is a way of managing stress, building resilience, and improving our mental and physical health. Self-care is not just about pampering ourselves or doing things that feel good for a short period of time; it is about investing in a lifestyle that promotes long-term wellbeing.
+
+//2. Make time for yourself
+//Creating time and space to take care of yourself is essential. Make sure to set aside time each day—even if it’s just 15 minutes—for some “me time”. This could include meditation, yoga, a walk in nature, listening to music, journaling, or anything else that brings you joy and peace.
+
+//3. Connect with yourself
+//Take time to connect with yourself and check in with how you’re feeling—physically, emotionally, and mentally. Ask yourself questions about your needs and feelings. Journaling can be a good way to explore this further and gain insight into yourself.
+
+//4. Practice mindfulness
+//Mindfulness is the practice of being aware of the present moment without judgment. By being mindful, we can cultivate a greater sense of self-awareness and understanding. We can also learn to better manage difficult emotions and life’s stresses.
+
+//5. Eat well
+//Eating nutritious and balanced meals is essential for physical and mental wellbeing. Stock up on healthy foods and incorporate a variety of fruits, vegetables, proteins, and grains into your diet. Avoid processed foods and snacks that are high in sugar and saturated fat.
+
+//6. Exercise regularly
+//Exercising regularly not only benefits our physical health, but our mental health as well. This could mean anything from a brisk walk, cycling, swimming, or even stretching. Find something that works for you and make sure to fit it into your weekly routine.
+
+//7. Get enough sleep
+//Getting enough sleep is important for our physical and mental wellbeing. Sleep deprivation can increase stress levels and impair our ability to concentrate. It’s recommended that adults get 7-8 hours of sleep each night.
+
+//8. Take time for relaxation
+//Relaxation can be anything from a hot bath, reading a book, listening to music, or taking a nap. Make sure to fit in some relaxation time each day and give yourself permission to do something you enjoy.
+
+//9. Seek support
+//If you’re feeling overwhelmed or struggling with difficult emotions, don’t be afraid to seek outside help. Talking to a friend, family member, or therapist can help you process your feelings and can be an invaluable source of support.
+
+//10. Reframe negative thinking
+//It’s important to recognize and challenge negative thinking patterns and reframe them in a more positive light. Instead of saying “I can’t do this”, try saying “I’ll give it my best shot”. Acknowledge your self-doubts but don’t let them stop you from achieving your goals.
